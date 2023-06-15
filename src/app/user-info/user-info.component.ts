9a5df696155419b52d9ab55c38dabd520db50c04
fedediclaudio <federicodicla@gmail.com>
2023-06-15 18:04:12 -0300

    Implementación formularios

--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Bank } from 'src/model/Bank';
 import { Card } from 'src/model/Card';
 import { Owner } from 'src/model/Owner';
 
@@ -9,13 +10,19 @@ import { Owner } from 'src/model/Owner';
 })
 export class UserInfoComponent {
 
+  banks: Bank[];
   owner: Owner;
 
   constructor() {
+    let santander: Bank = new Bank("Santander", "Bartolomé Mitre 480")
+    let nacion: Bank = new Bank("Nacion", "Bartolomé Mitre 480")
+    let ciudad: Bank = new Bank("Ciudad", "Av. Cabildo 3061")
+    this.banks = [];
+    this.banks.push(santander, nacion, ciudad);
     this.owner = new Owner("Jhon Doe", 30, "12/09/2015")
-    let card1: Card = new Card("Tajeta 1", "1111 1222 1333 4444", "Debito", "Santander", "06/26")
-    let card2: Card = new Card("Tajeta 2", "5555 6666 7777 8888", "Debito", "Nacion", "08/29")
-    let card3: Card = new Card("Tajeta 3", "2222 2333 2444 9999", "Debito", "BBVA", "02/24")
+    let card1: Card = new Card("Tajeta 1", "1111 1222 1333 4444", "Debito", santander, "06/26")
+    let card2: Card = new Card("Tajeta 2", "5555 6666 7777 8888", "Debito", nacion, "08/29")
+    let card3: Card = new Card("Tajeta 3", "2222 2333 2444 9999", "Debito", ciudad, "02/24")
     this.owner.addCard(card1);
     this.owner.addCard(card2);
     this.owner.addCard(card3);
--- src/model/Card.ts
+++ src/model/Card.ts
@@ -1,11 +1,13 @@
+import { Bank } from "./Bank";
+
 export class Card {
     name: string;
     number: string;
     type: string;
-    bank: string;
+    bank: Bank;
     expiredDate: string;
   
-    constructor(name: string, number: string, type: string, bank: string, expiredDate: string) {
+    constructor(name: string, number: string, type: string, bank: Bank, expiredDate: string) {
       this.name = name;
       this.number = number;
       this.type = type;
