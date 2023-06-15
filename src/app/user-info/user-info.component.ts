b9f5cb41b0b5d19c50b55a8f8d6c67801fdc0f92
fedediclaudio <federicodicla@gmail.com>
2023-06-15 20:18:49 -0300

    Complete Reactive Forms

--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Bank } from 'src/model/Bank';
 import { Card } from 'src/model/Card';
 import { Owner } from 'src/model/Owner';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-info',
@@ -14,10 +14,14 @@ export class UserInfoComponent {
   banks: Bank[];
   owner: Owner;
 
-  name= new FormControl('');
-  type = new FormControl('');
-  bank = new FormControl('');
-  expireDate = new FormControl('');
+  newBankForm = new FormGroup({
+    name:  new FormControl(),
+    number: new FormControl(),
+    type: new FormControl(),
+    bank: new FormControl(),
+    expireDate: new FormControl()
+  })
+  
 
   constructor() {
     let santander: Bank = new Bank("Santander", "Bartolomé Mitre 480")
@@ -32,6 +36,19 @@ export class UserInfoComponent {
     this.owner.addCard(card1);
     this.owner.addCard(card2);
     this.owner.addCard(card3);
+  }
+
+  onSubmit() {
+    let name = this.newBankForm.get('name')?.value
+    let number = this.newBankForm.get('number')?.value
+    let type = this.newBankForm.get('type')?.value
+    let bank = this.newBankForm.get('bank')?.value
+    let expireDate = this.newBankForm.get('expireDate')?.value
+    console.log(bank);
+
+    let card: Card = new Card(name, number, type, bank, expireDate);
+
+    this.owner.addCard(card);
   }
 
 }
